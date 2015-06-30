5cd4f1082ed1c5fbbf8fe1565f99248711aa40e4
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-30 19:15:20 -0400

    Move static data to posts-index property

--- app/components/posts-index.js
+++ app/components/posts-index.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['posts-list']
+  classNames: ['posts-list'],
+
+  posts: [
+    {title: 'Title 1', url: 'http://google.com'},
+    {title: 'Title 2', url: 'http://google.com'},
+    {title: 'Title 3', url: 'http://google.com'}
+  ]
 });
