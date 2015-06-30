bae473bdc529fc9d65ba8fc6bfaf569143560343
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-30 18:52:09 -0400

    Move HTML to posts-index component

---
+++ tests/unit/routes/pins/index-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:pins/index', 'Unit | Route | pins/index', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
