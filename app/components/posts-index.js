b0d319ae4a5ce08cca6b97ba21d5e31afd61124f
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-30 20:12:58 -0400

    Move static data to route

--- app/components/posts-index.js
+++ app/components/posts-index.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['posts-list'],
-
-  posts: [
-    {title: 'Title 1', url: 'http://google.com'},
-    {title: 'Title 2', url: 'http://google.com'},
-    {title: 'Title 3', url: 'http://google.com'}
-  ]
+  classNames: ['posts-list']
+  
 });
--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return [
+      {title: 'Title 1', url: 'http://google.com'},
+      {title: 'Title 2', url: 'http://google.com'},
+      {title: 'Title 3', url: 'http://google.com'}
+    ];
+  }
 });
