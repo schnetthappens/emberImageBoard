d2ad0827f669e092e326983cfe67a8812a2a659e
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-30 22:19:04 -0400

    Refactor as images, not blog posts

--- app/models/post.js
+++ app/models/post.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  url: DS.attr('string') 
+  url: DS.attr('string'),
+  caption: DS.attr('string') 
 });
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('posts', function() {
     this.route('create');
+    this.route('show', {path: '/post:_id'});
   });
 });
 
